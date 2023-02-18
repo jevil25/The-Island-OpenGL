335d54882dafd1a7893bc16c779f9bbbad8fc2b1
AaronNazareth <4nm20cs005@nmamit.in>
2023-02-18 14:44:44 +0530

    optimized:used arguments to reduce code line

--- main.cpp
+++ main.cpp
@@ -26,33 +26,24 @@ void Button(){
     glEnd();
 }
 
-void display(){
+void display(char *text,int length,int x,int y){
+    int i;
+    for(i=0;i<length;i++){
+        glRasterPos2i(x+i,y);
+        glColor3b(1,1,1);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+    }
+}
+
+void drawMyDesign(){
     char ajn[]="Aaron Jevil Nazareth   4nm20cs005";
     char afd[]="Aaron Francis Dsouza   4nm20cs004";
     char projectName[]="The Island";
     int i,name_usn_length=33, projectName_length=10;
-    for(i=0;i<projectName_length;i++){
-        glRasterPos2i(20+i,45);
-        glColor3b(1,1,1);
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,projectName[i]);
-    }
-    for(i=0;i<name_usn_length;i++){
-        glRasterPos2i(10+i,40);
-        glColor3b(1,1,1);
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,ajn[i]);
-    }
-    for(i=0;i<name_usn_length;i++){
-        glRasterPos2i(10+i,38);
-        glColor3b(1,1,1);
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,afd[i]);
-    }
-    Button();
-    glFlush();
-}
-
-void drawMyDesign(){
     glClear(GL_COLOR_BUFFER_BIT);
-    display();
+    display(projectName,projectName_length,20,45);
+    display(ajn,name_usn_length,10,40);
+    display(afd,name_usn_length,10,38);
     Button();
 }
 
