177399a7db90e3faadf50417d46d641f5f25dbbb
AaronNazareth <4nm20cs005@nmamit.in>
2023-02-18 13:34:31 +0530

    added button to display

--- main.cpp
+++ main.cpp
@@ -7,12 +7,30 @@ void init(void){
     glOrtho(0,50,0,50,0,10);
 }
 
+void Button(){
+    char next[]="next";
+    int next_length=4,i;
+    glColor3b(1,1,1);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(22,30);
+    glVertex2f(22,34);
+    glVertex2f(26,34);
+    glVertex2f(26,30);
+    glEnd();
+    for(i=0;i<next_length;i++){
+        glRasterPos2f(22.25+i,31.75);
+        glColor3b(1,1,1);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,next[i]);
+    }
+    glFlush();
+    glEnd();
+}
+
 void display(){
     char ajn[]="Aaron Jevil Nazareth   4nm20cs005";
     char afd[]="Aaron Francis Dsouza   4nm20cs004";
     char projectName[]="The Island";
     int i,name_usn_length=33, projectName_length=10;
-    glClear(GL_COLOR_BUFFER_BIT);
     for(i=0;i<projectName_length;i++){
         glRasterPos2i(20+i,45);
         glColor3b(1,1,1);
@@ -28,8 +46,14 @@ void display(){
         glColor3b(1,1,1);
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,afd[i]);
     }
-    glEnd();
+    Button();
     glFlush();
+}
+
+void drawMyDesign(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    display();
+    Button();
 }
 
 int main(int argc, char**argv)
@@ -37,10 +61,11 @@ int main(int argc, char**argv)
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(10,10);
-    glutInitWindowSize(900,500);
+    glutInitWindowSize(1000,1000);
     glutCreateWindow("The Island");
     init();
-    glutDisplayFunc(display);
+    glutDisplayFunc(drawMyDesign);
+    Button();
     glutMainLoop();
     return 0;
 }
